0950914cb73eaa4b18b1936964e22347a00936c5
George <tvproducermexico@mail.com>
2020-10-07 11:51:28 -0400

    Updates

---
+++ precache-manifest.ff91618071d5ee671a6c003d2c1c73ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b811a448e83c4aac25fe22b521f608",
+    "url": "/Jorge-site/index.html"
+  },
+  {
+    "revision": "67f19f46caad1f3061e4",
+    "url": "/Jorge-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a84332dc28e0b2a7622a",
+    "url": "/Jorge-site/static/js/2.e862df64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jorge-site/static/js/2.e862df64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f19f46caad1f3061e4",
+    "url": "/Jorge-site/static/js/main.dedb96ea.chunk.js"
+  },
+  {
+    "revision": "d4db09296ad01a87607e",
+    "url": "/Jorge-site/static/js/runtime-main.9e376f7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jorge-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
