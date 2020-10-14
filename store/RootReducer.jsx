3af525d5cb84f2cc529d53118aa5ec7f1811f5f9
Jorge Melchor <48293166+JMartinRutgers@users.noreply.github.com>
2020-10-14 11:09:40 -0400

    Delete RootReducer.jsx

--- store/RootReducer.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import DashboardReducer from "./DashboardReducer";
-
-const rootReducer = combineReducers({
-  DashboardReducer
-});
-
-export default rootReducer;
