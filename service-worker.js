d5e210ffa6759377e38bf654d4bef65ce99da234
George <tvproducermexico@mail.com>
2020-10-15 09:23:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jorge-site/precache-manifest.67e11704cca14f94d8eed45adeb57477.js"
+  "/Jorge-site/precache-manifest.12096d75306276ae29e3096cd9c10c67.js"
 );
 
 self.addEventListener('message', (event) => {
